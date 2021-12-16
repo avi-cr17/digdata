39fc5a8638e693f2715d1a345dfdc3222cef47c4
Avinandan <avinandan.agt@gmail.com>
2021-12-17 03:16:13 +0530

    top div completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+
+import { BrowserRouter} from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+      <Nav/>
+      <div className='top-div  '>
+        <div className='w-full lg:h-4/6 h-4/6 flex justify-end items-center flex-col'>
+        <h1 className='lg:w-2/3 w-5/6'>Fascinating tool to solve all your data entry problems</h1>
+        <h2  className='lg:w-2/3 w-5/6 mt-8'>easy and effective way to automate data entry and grow your business</h2>
+        <button className="animation text-col w-auto pr-5 pl-5 rounded-3xl m-10 h-auto pt-2 pb-3 lg:text-xl text-lg text-center">start your journey</button>
+        </div>
+        <div className='flex w-full  justify-center h-1/12 items-center'>
+        <img src="https://assets.website-files.com/602f5458a0b1d868c6c84d5b/60b72e8c428fbb080fbecdaa_Group%20342.svg" className='img-animated' alt='animated image' />
+        </div>      
+      </div>
+      
+      <div className='shadow'>
+      </div>
+      <div className='below-shadow'>
+
+      </div>
+      <div className='h-screen'>
+
+      </div>
+      </BrowserRouter>
+    
+    
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => {
+const [click, setClick] = React.useState(false);
+
+const handleClick = () => setClick(!click);
+const Close = () => setClick(false);
+
+const [navstate,Setnavstate]=useState("navbar")
+
+  useEffect(()=>{
+     document.addEventListener("scroll", e => {
+      
+      if (window.scrollY >= 50) {
+        if (navstate !== "nav-sticky") {
+          Setnavstate("nav-sticky")
+        }
+      } else {
+        if (navstate !== "navbar") {
+          Setnavstate("navbar")
+        }
+      }
+    })
+   
+    
+  },[navstate])
+
+
+return (
+  <div>
+   <div className={click ? "main-container" : ""} onClick={()=>Close()} />
+    <nav className={navstate+" shadow-2xl shadow-black"} onClick={(e)=>e.stopPropagation()}>
+      <div className="nav-container">
+        <NavLink exact to="/" className="nav-logo">
+          DigData
+          <i class="fa fa-database" aria-hidden="true"></i>
+        </NavLink>
+
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active"
+              className="nav-links"
+              onClick={click ? handleClick : null}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/about"
+              activeClassName="active"
+              className="nav-links"
+              onClick={click ? handleClick : null}
+            >
+              Products
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/blog"
+              activeClassName="active"
+              className="nav-links"
+              onClick={click ? handleClick : null}
+            >
+              Pricing
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/contact"
+              activeClassName="active"
+              className="nav-links"
+             onClick={click ? handleClick : null}
+            >
+              Contact Us
+            </NavLink>
+          </li>
+        </ul>
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+        </div>
+      </div>
+    </nav>
+  </ div>
+        
+    )
+}
+
+export default Nav
